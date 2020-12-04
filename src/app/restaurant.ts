eaa801a90862a3b0ce6adc742e83d8f4714fda07
Bilal Hoor <171085@ppu.edu.ps>
2020-12-04 18:23:49 +0200

    edit form

--- src/app/restaurant.ts
+++ src/app/restaurant.ts
@@ -1,8 +1,6 @@
 export class Restaurant {
 
-  id = -1;
   constructor(public name: string, public city: string, public street: string,
-    public latitude?: number, public longitude?:number, public image?:string, id?: number) {
-    this.id += 1;
+    public latitude?: number, public longitude?:number, public image?:string) {
   }
 }
