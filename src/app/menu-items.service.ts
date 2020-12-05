ac1f247581d2e40c134e509b3d4f40eef9341b7e
Bilal Hoor <171085@ppu.edu.ps>
2020-12-05 23:36:44 +0200

    SWE

--- src/app/menu-items.service.ts
+++ src/app/menu-items.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { from } from 'rxjs';
 import { MenuItem } from './menu-item';
 @Injectable({
   providedIn: 'root'
 })
 export class MenuItemsDataService {
-  Menu_Items:MenuItem[]=[
+  menuItems:MenuItem[]=[
     new MenuItem("odaiMI",88,"jklhlkhl","img1",0,0,5,false),
     new MenuItem("odaiMI",99,"jklhlkhl","img2",1,0,5,false)
 ];
@@ -14,27 +13,26 @@ export class MenuItemsDataService {
 
   getMenuItems():MenuItem[]
   {
-    return this.Menu_Items;
+    return this.menuItems;
   }
   addMenuItem(MI:MenuItem){
     
-    MI.id = this.Menu_Items.length;
+    MI.id = this.menuItems.length;
     MI.ordered= false;
-    this.Menu_Items.push(MI);
+    this.menuItems.push(MI);
     console.log("from seervice " + MI.id+MI.descr+MI.name);
   }
   getMenuItemsOfRest(res_id:number):MenuItem[]
   {
     let MI:MenuItem[]=[];
     let MI_id:number = 0;
-    for(let i=0;i<this.Menu_Items.length;i++)
+    for(let i=0;i<this.menuItems.length;i++)
     {
-      if(this.Menu_Items[i].rest_id==res_id)
+      if(this.menuItems[i].rest_id==res_id)
       {  
          
-        this.Menu_Items[i].id = MI_id;
-        MI.push(this.Menu_Items[i]);
-        console.log("item with id "+this.Menu_Items[i].id+"pushed to res with id "+this.Menu_Items[i].rest_id+"");
+        this.menuItems[i].id = MI_id;
+        MI.push(this.menuItems[i]);
         MI_id++;
       }
     }
@@ -42,21 +40,21 @@ export class MenuItemsDataService {
   }
   getMenuItem(res_id:number,id:number):MenuItem
   {
-    for(let i=0;i<this.Menu_Items.length;i++)
+    for(let i=0;i<this.menuItems.length;i++)
     {
-      if((this.Menu_Items[i].rest_id==res_id) &&(this.Menu_Items[i].id==id) )
+      if((this.menuItems[i].rest_id==res_id) &&(this.menuItems[i].id==id) )
       {  
-        return this.Menu_Items[i];
+        return this.menuItems[i];
       }
     }
   }
   setMenuItem(resid:number,id:number,MI:MenuItem)
   {
-    for(let i=0;i<this.Menu_Items.length;i++)
+    for(let i=0;i<this.menuItems.length;i++)
     {
-      if((this.Menu_Items[i].rest_id==resid) && (this.Menu_Items[i].id==id))
+      if((this.menuItems[i].rest_id==resid) && (this.menuItems[i].id==id))
       {
-        this.Menu_Items[i]= MI;
+        this.menuItems[i]= MI;
       }
     }
   }
