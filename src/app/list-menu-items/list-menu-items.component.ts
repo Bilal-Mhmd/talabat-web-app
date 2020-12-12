d57f605e1953db75157d1c02704c0e303c15ff60
Bilal Hoor <171085@ppu.edu.ps>
2020-12-12 23:36:47 +0200

    toggle button & show orders

--- src/app/list-menu-items/list-menu-items.component.ts
+++ src/app/list-menu-items/list-menu-items.component.ts
@@ -13,22 +13,25 @@ import { OrdersListDataService } from '../services/orders-list-data.service';
 export class ListMenuItemsComponent implements OnInit {
 
   _menu_items : MenuItem[]=[];
-
-
-_id:number;
+  _id:number;
 
 constructor(
   private route: ActivatedRoute,
   private _router: Router,
-  private MenuItemsService:MenuItemsDataService,
-  private OrdersListService:OrdersListDataService){}
+  private menuItemsService : MenuItemsDataService,
+  private ordersListService:OrdersListDataService){}
 
   ngOnInit(): void {
 
     this.route.params.subscribe(
         (params: Params) => {this._id = +params['resid'];}
       );
-      this._menu_items=this.MenuItemsService.getMenuItemsOfRest(this._id);
+    this._menu_items = this.menuItemsService.getMenuItemsOfRest(this._id);
+    
+    this._menu_items = this.menuItemsService.getMenuItems();
+    this._menu_items.forEach(function (menu, index) {
+      menu.id = index;
+    });
   }
 
   editMenuItem(id:number)
@@ -40,17 +43,24 @@ constructor(
     this._router.navigateByUrl(`/add_item/${this._id}`);
     console.log("navigation to menu item with res id "+this._id);
   }
-  orderMenuItem(id: number) {
+  orderMenuItem(id: number, element) {
+    this.menuItemsService.menuItems[id].ordered = true;
+    let newOrder: MenuItem = this._menu_items[id];
+    this.ordersListService.Orders.push(newOrder);
+    //element.textContent = "Cancel Order";
+  }
 
-    this.MenuItemsService.menuItems[this._id].ordered = true;
-    let _MenuItem: MenuItem = this.MenuItemsService.getMenuItem(this._id, id);
-    this.OrdersListService.addOrder(_MenuItem);
+  deleteOrderMenuItem(id: number, element) {
+    this.menuItemsService.menuItems[id].ordered = false;
+    let delOrder: MenuItem = this._menu_items[id];
+    this.ordersListService.Orders.splice(id, 1);
+    //element.textContent = "Order";
+
 
   }
-  deleteOrderMenuItem(id: number) {
-    let _MenuItem:MenuItem = this.MenuItemsService.getMenuItem(this._id,id);
-    this.OrdersListService.deleteOrder(_MenuItem);
 
+  showOrders() {
+    this._router.navigateByUrl(`list_orders/${this._id}`)
   }
 
 }
